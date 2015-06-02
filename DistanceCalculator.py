f11a2a5961d05591383ee3fdbe5bcce4df23a02b
rajbin <rajbinmanandhar@hotmail.com>
2015-06-02 18:08:31 +0545

    Added Files

---
+++ DistanceCalculator.py
@@ -0,0 +1,27 @@
+from Tour import Tour
+
+def displayOption():
+    firstTour = Tour("New York, NY", "Lansing, MI")
+    secondTour = Tour("Oakland, CA")
+    thirdTour = Tour("Sacramento, CA", "Oakland, CA")
+
+    print("""\nFirst Tour:{0}
+Second Tour:{1}
+Third Tour:{2}\n""".format(firstTour, secondTour, thirdTour))
+
+    print("First tour distances:\n\tDriving: {0} km;\n\tBiking: {1} km;\n\tWalking: {2} km".format(
+    round(firstTour.getDistance("driving")/1000), round(firstTour.getDistance('bicycling')/1000),
+    round(firstTour.getDistance('walking')/1000)))
+
+    print("\nSecond tour distances:\n\tDriving: {0} km;\n\tBiking: {1} km;\n\tWalking: {2} km".format(
+    round(secondTour.getDistance("driving")/1000), round(secondTour.getDistance('bicycling')/1000),
+    round(secondTour.getDistance('walking')/1000)))
+
+    print("\nThird tour distances:\n\tDriving: {0} km;\n\tBiking: {1} km;\n\tWalking: {2} km".format(
+    round(thirdTour.getDistance("driving")/1000), round(thirdTour.getDistance('bicycling')/1000),
+    round(thirdTour.getDistance('walking')/1000)))
+    
+def main():
+    displayOption()
+
+main()
---
+++ Tour.py
@@ -0,0 +1,44 @@
+import urllib2
+from xml.etree import ElementTree
+
+class Tour(object):
+    """An instance of this class is to be instantiated with an arbitrary 
+    number of US cities, and is used to fetch information from the web."""
+
+    __googleAPI = "http://maps.googleapis.com/maps/api/distancematrix/xml"
+
+    def __init__(self, *cities, **kwarg):
+        self._lstCity = []
+        for city in cities:
+            self._lstCity.append(city)  
+               
+    def getDistance(self, mode="driving"):
+        totalDistance = 0
+        counter, nextCounter = 0,0
+        
+        for counter in range(0, len(self._lstCity) - 1):
+            nextCounter = counter + 1
+            url = self.__googleAPI
+            url = url + "?origins={0}&destinations={1}&mode={2}&sensor=false".format(self._lstCity[counter].replace(" ","+").replace(",",""), self._lstCity[nextCounter].replace(" ","+").replace(",",""), mode)
+            
+            try:
+                response = urllib2.urlopen(url).read()
+                xmlResponse = ElementTree.fromstring(response)
+
+                distance = float(xmlResponse.find("row").find("element").find("distance").findtext("value"))
+                totalDistance += distance
+
+            except urllib2.HTTPError, err:
+                if err.code == 404:
+                       print "Page not found!"
+                elif err.code == 403:
+                    print "Access denied!"
+                else:
+                    print "An error has occured. Please try after sometime."
+            except urllib2.URLError, err:
+                print "An error has occured. Please try after sometime."
+        
+        return totalDistance
+
+    def __str__(self):
+        return str(self._lstCity)
